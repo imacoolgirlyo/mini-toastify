ce9119f1743a5546de8a3901a976779c73b9ed4f
Seokyung Jung <tjrud1438@naver.com>
2021-11-26 00:45:12 +0900

    dispatchToast에서 lazy = true 일때는 별도로 toast.configure를 설정했을 떄이다. lazy가 아니면 queue에 바로 쌓인다. 다음 시간엔 이걸..

--- src/core/toast.tsx
+++ src/core/toast.tsx
@@ -1,8 +1,16 @@
-import { ToastContent, NotValidatedToastProps, ToastOptions, Id } from '../types'
+import { ToastContent, NotValidatedToastProps, ToastOptions, Id, ToastContainerProps } from '../types'
 import { isNum, isStr, TYPE} from '../utils'
 import { ContainerInstance } from '../hooks'
 
+interface EnqueuedToast {
+  content: ToastContent
+  options: NotValidatedToastProps
+}
+
 let containers = new Map<ContainerInstance | Id, ContainerInstance>();
+let containerConfig: ToastContainerProps;
+let queue: EnqueuedToast[] = []
+let lazy = false;
 
 function isAnyContainerMounted(){
   return containers.size > 0 // parameter를 쓰지 않고 전역변수를 사용함. 이 기준을 뭘까
@@ -30,10 +38,18 @@ function mergeOptions(type: string, options?: ToastOptions){
 
 function dispatchToast(content:ToastContent, options: NotValidatedToastProps): Id {
   if(isAnyContainerMounted()){
-    
+    // [ ] TO DO
   }else {
+    queue.push({ content, options }) 
+    if(lazy && canUseDom){ 
+      /**
+       * 최초에 lazy = true가 되는 시점은 toast.configure인데 얘가 어디서 호출되는지 모르겠다.
+       * 이건 컴포넌트가 lazy mount 일 때만 사용되는듯!
+       */ 
 
+    }
   }
+
 }
 
 const toast = (content: ToastContent, options?: ToastOptions) => { 
@@ -42,5 +58,9 @@ const toast = (content: ToastContent, options?: ToastOptions) => {
   //dispatchToast(content)
 }
 
+toast.configure = (config: ToastContainerProps = {}) => {
+  lazy = true
+  containerConfig = config
+}
 
 export { toast }
